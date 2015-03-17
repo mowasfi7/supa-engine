3b25f5f37e45f45e3b545681306173abaa3a7d88
woollyblanket <amandadurkan@gmail.com>
2015-03-17 14:32:20 +0000

    Separate the routes into its own file

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -10,89 +10,14 @@
  */
  angular
  .module('clientApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'ui.router',
-    'ui.bootstrap'
-    ])
+ 	'ngAnimate',
+ 	'ngCookies',
+ 	'ngResource',
+ 	'ngRoute',
+ 	'ngSanitize',
+ 	'ngTouch',
+ 	'ui.router',
+ 	'ui.bootstrap'
+ 	]);
 
- .config(function($stateProvider, $urlRouterProvider) {
-
-    $stateProvider
-    .state('home', {
-        url: '/',
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        abstract: true
-    })
-    .state('about', {
-        url: '/about',
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        abstract: true
-    })
-    .state('profile', {
-        url: '/profile',
-        templateUrl: 'views/profile.html',
-        controller: 'ProfileCtrl'
-    })
-    .state('signup', {
-        url: '/signup',
-        templateUrl: 'views/signup.html',
-        controller: 'SignupCtrl'
-    })
-
-    .state('notfound', {
-        url: '/notfound',
-        templateUrl: 'views/notfound.html'
-    })
-
-  // nested states
-  // each of these sections will have their own view
-  .state('home.location', {
-    url: '',
-    templateUrl: 'views/form-location.html'
-  })
-
-  .state('home.shoppinglist', {
-    url: '',
-    templateUrl: 'views/form-shoppinglist.html'
-  })
-
-  .state('about.theApp', {
-    url: '/theapp',
-    templateUrl: 'views/about-theapp.html'
-  })
-
-  .state('about.wasfi', {
-    url: '/wasfi',
-    templateUrl: 'views/about-wasfi.html'
-  })
-
-  .state('about.marco', {
-    url: '/marco',
-    templateUrl: 'views/about-marco.html'
-  })
-
-  .state('about.amanda', {
-    url: '/amanda',
-    templateUrl: 'views/about-amanda.html'
-  })
-
-  .state('profile.options', {
-    url: '/options',
-    templateUrl: 'views/profile-options.html'
-  })
-
-  .state('profile.personal', {
-    url: '/personal',
-    templateUrl: 'views/profile-personal.html'
-  });
-
-  $urlRouterProvider.otherwise('/notfound');
-});
 
