434d55ca58cf09169408ba4a77d804e77e21d352
Mohamed Wasfi <mohamed.wusfi@gmail.com>
2016-02-18 13:46:20 +0000

    Allow localhost cross origin

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ if (app.get('env') == 'production') {
 
 app.use(function (req, res, next) {
 	console.log('Setting Allow Origin');
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
